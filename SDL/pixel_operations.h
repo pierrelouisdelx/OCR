227017fc9a96553da601c09524d8ca929ee6ae3a
Adam Britel <adam.britel@epita.fr>
2020-10-07 15:53:10 +0200

    op sur les pixels

---
+++ SDL/pixel_operations.h
@@ -0,0 +1,11 @@
+#ifndef PIXEL_OPERATION_H_
+#define PIXEL_OPERATION_H_
+
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+Uint32 get_pixel(SDL_Surface *surface,unsigned x, unsigned y);
+void put_pixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
+void update_surface(SDL_Surface* screen,
+        SDL_Surface* screen, SDL_Surface* image);
+#endif
