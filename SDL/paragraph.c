8d239869666a9edae7df4b5c198f245d0191811c
eloi.honnet <eloi.honnet@epita.fr>
2020-10-14 15:51:25 +0200

    paragraph 3/4 done

--- SDL/paragraph.c
+++ SDL/paragraph.c
@@ -7,39 +7,55 @@ SDL_Surface* paragraph_reco(SDL_Surface* image)
 {
     int h = image->h;
     int w = image->w;
-    int min_i;
-    int min_j;
-    int max_i;
-    int max_j;
-    for(int i=0; i < w; i++)
+    int min_i = h - 1;
+    int min_j = w - 1;
+    int max_i = 0;
+    int max_j = 0;
+    int black = 0;
+    printf("h : %i,\n w: %i\n",h,w); //prints h and w
+    for(int i = 0; i < h; i++)
     {
-        for(int j=0; j < h; j++)
+        for(int j = 0; j < w; j++)
         {
             Uint32 pixel = get_pixel (image,i,j);
             Uint8 r, g, b;
             SDL_GetRGB(pixel, image->format, &r, &g, &b);
-            if (r > 0 && i < min_i)
+            if (r == 0 && g == 0 && b == 0)
+                black = 1;
+            else
+                black = 0;
+            if (black && i < min_i && j < h)
+            {
                 min_i = i;
-            if (r > 0 && j < min_j)
+            }
+            if (black && j < min_j && i < w)
+            {
                 min_j = j;
-            if (r > 0 && i > max_i)
+            }
+            if (black && i > max_i && i < w)
+            {
                 max_i = i;
-            if (r > 0 && i > max_j)
+            }
+            if (black && j > max_j && j < h)
+            {
                 max_j = j;
+            }
         }
     }
-    for(int i=min_i; i < max_i ; i++)
+    printf("\nimax: %i, \njmax: %i, \n imin: %i, \n jmin: %i \n",max_i,max_j,min_i,min_j);
+    for(int i = min_i; i <= max_i; i++)
     {
         Uint32 new_pixel = SDL_MapRGB(image->format, 255, 0, 0);
         put_pixel(image,i,min_j,new_pixel);
         put_pixel(image,i,max_j,new_pixel);
     }
-    for(int j=min_j; j < max_j ; j++)
+    for(int j=min_j; j <= max_j; j++)
     {
         Uint32 new_pixel = SDL_MapRGB(image->format, 255, 0, 0);
         put_pixel(image,min_i,j,new_pixel);
         put_pixel(image,max_i,j,new_pixel);
     }
+
     return image;
 }
 
