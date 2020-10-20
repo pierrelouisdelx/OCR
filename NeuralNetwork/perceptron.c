091fb16f1a7b50e3c36b4e6a8b6d8a3edef0a9d6
Your Name <you@example.com>
2020-10-20 21:34:48 +0200

    rotation and traning for neural network

---
+++ NeuralNetwork/perceptron.c
@@ -0,0 +1,9 @@
+int weight = 2;
+int bias = 3;
+
+
+int x1 = 1;
+int x2 = 1;
+int perceptron = x1 * weight + x2 * weight + bias;
+
+xor = perceptron xor perceptron;
---
+++ NeuralNetwork/softmax.h
@@ -0,0 +1,6 @@
+#ifndef SOFTMAX_H
+#define SOFTMAX_H
+
+double softmax(double factor);
+
+#endif
