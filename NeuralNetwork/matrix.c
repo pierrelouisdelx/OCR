6f44208e31fa7520135e14011af269faa9653cef
geraud.del-pino <geraud.del-pino@epita.fr>
2020-10-24 21:10:24 +0200

    added save and load data to load and save data to a given file, check comments for further explainations

--- NeuralNetwork/matrix.c
+++ NeuralNetwork/matrix.c
@@ -94,3 +94,54 @@ void function_matrix(double (*f)(double), double m[][2])
         }
     }
 }
+
+
+void SaveData(double matrix,long path) //path must be absolute
+{
+  FILE* file = fopen(path, "w"); //w allows us to write in the file
+  if(file == NULL)
+  {
+    printf("matrix.c : SaveData, no such file exists");
+    exit(1);
+  }
+  fprintf(file, "%f\n", line)
+  fprintf(file, "%f\n", column) 
+  for(int i = 0; i < 2; ++i) //writes each bits of data in the file
+  {
+    for(int h = 0; h < 2; ++h)
+    {
+      fprintf(file, "%f\n", matrix[i][h]);
+    }
+  }
+  fclose(file);
+  //in the for loop the 2 represents the number of input and hidden layers
+}
+//the first two lines are the dimension of the matrix, the following lines are just
+//the values stored inside of it
+
+double LoadData(long path)
+{
+  FILE* file = fopen(path, 'r');
+  int SizeLineMax = 15;//this should be changed later on
+  char  *line = calloc(15, sizeof(char)); //this should be changed later as the needs changes
+  double matrix[2][2];
+
+  if(file == NULL)
+  {
+    printf("matrix.c : LoadData, no such file exists");
+    exit(1);
+  }
+  for(int i = 0; i < 2; ++i)
+  {
+    for(int h = 0; i < 2; ++h)
+    {
+      fgets(line, SizeLineMax, file);
+      strok(line, "\n");
+      matrix[i][h] = atof(line);
+    }
+  }
+
+  fclose(file);
+  return matrix;
+}
+
