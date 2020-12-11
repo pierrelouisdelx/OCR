680277ce302aab9f16493432742027438b144e6d
Your Name <you@example.com>
2020-12-11 19:24:45 +0100

    image to matrix

--- SDL/sdl.h
+++ SDL/sdl.h
@@ -1,14 +1,17 @@
 #ifndef SDL_H
 #define SDL_H
 
+#include <SDL2/SDL.h>
+
 void init_sdl();
 void on_keypress();
 SDL_Surface* load_image(char *file);
+void image_to_matrix(char *path, double matrix[]);
 SDL_Surface* display_image(SDL_Surface *image);
 SDL_Surface* blackwhite(SDL_Surface* image);
 SDL_Surface* grayscale(SDL_Surface* image);
 SDL_Surface* noisecancel(SDL_Surface* image);
-SDL_Surface* resize(SDL_Surface *Surface, Uint16 Width, Uint16 Height);
 SDL_Surface* rotate(SDL_Surface* image, int angle);
+SDL_Surface* resize(SDL_Surface* image);
 
 #endif
