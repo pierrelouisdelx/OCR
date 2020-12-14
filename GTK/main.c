2d871a2fa8c0761d08a5ca29a1107442842049ef
Your Name <you@example.com>
2020-12-14 13:37:33 +0100

    gtk image display and resize working

--- GTK/main.c
+++ GTK/main.c
@@ -1,35 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 #include <gtk/gtk.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+
 #include "../SDL/sdl.h"
 #include "../SDL/pixel_operations.h"
 #include "../SDL/paragraph.h"
-#include <math.h>
-#include <stdio.h>
 
-void open_file(GtkWindow *window, GtkWidget *image)
-{
-    GtkWidget *dialog;
-    GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
-    gint res;
-
-    dialog = gtk_file_chooser_dialog_new("Open File", NULL,
-                action, ("_Cancel"), GTK_RESPONSE_CANCEL,("_Open"),
-                GTK_RESPONSE_ACCEPT,NULL);
-    
-    res = gtk_dialog_run(GTK_DIALOG(dialog));
-    if (res == GTK_RESPONSE_ACCEPT)
-    {
-        char *filename;
-        GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
-        filename = gtk_file_chooser_get_filename(chooser);
-        printf("%s", filename);
-        image = gtk_image_new_from_file(filename);
-        g_free (filename);
-    } 
-    gtk_widget_destroy (dialog);
-}
+#include "../NeuralNetwork/neuralnetwork.c"
+#include "../NeuralNetwork/row_matrix.c"
+#include "../NeuralNetwork/main.c"
 
 void css(void)
 {
@@ -64,92 +47,108 @@ void gtk_blackwhite(GtkWindow *window, char *file)
     display_image(image);
 }
 
-void gtk_segmentation(GtkWindow *window, char *file)
+void gtk_segmentation(GtkWindow *window, gpointer data)
 {
+    char *file = data;
     SDL_Surface *image = load_image(file);
     image = lines_reco(image);
     //lines_and_char_storage(image);
     display_image(image);
 }
 
-int main(int argc, char *argv[])
+typedef struct {
+    GtkWidget *w_dlg_file_choose;       // Pointer to file chooser dialog box
+    GtkImage *w_img_main;              // Pointer to image widget
+} app_widgets;
+
+// File --> Open
+void on_menuitm_open_activate(GtkMenuItem *menuitem, app_widgets *app_wdgts)
 {
-    //Areas
-    GtkWindow *window;
-    GtkWidget *vbox;
-    GtkWidget *hpaned;
-
-    GtkWidget *image;
-
-    //Buttons
-    GtkWidget *openfile;
-    GtkWidget *grayscale;
-    GtkWidget *blackandwhite;
-    GtkWidget *segmentation;
-    GtkWidget *OCR;
-    GtkWidget *text;
-    GtkWidget *save;
-    GtkWidget *quit;
-
-    char *img = "OCRtext3.bmp";
+    gchar *file_name = NULL; // Name of file to open from dialog box
     
-    init_sdl();
+    // Show the "Open Image" dialog box
+    gtk_widget_show(app_wdgts->w_dlg_file_choose);
+    
+    // Check return value from Open Image dialog box to see if user clicked the Open button
+    if (gtk_dialog_run(GTK_DIALOG (app_wdgts->w_dlg_file_choose)) == GTK_RESPONSE_OK) {
+        // Get the file name from the dialog box
+        file_name = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(app_wdgts->w_dlg_file_choose));
+        if (file_name != NULL) {
+            gtk_image_set_from_file(GTK_IMAGE(app_wdgts->w_img_main), file_name);
+        }
+        g_free(file_name);
+    }
+
+    // Finished with the "Open Image" dialog box, so hide it
+    gtk_widget_hide(app_wdgts->w_dlg_file_choose);
+}
+
+// File --> Quit
+void on_menuitm_close_activate(GtkMenuItem *menuitem, app_widgets *app_wdgts)
+{
+    gtk_main_quit();
+}
+
+// called when window is closed
+void on_window_main_destroy()
+{
+    gtk_main_quit();
+}
+
+int main (int argc, char *argv[])
+{
+    // Initializes GTK.
     gtk_init(NULL, NULL);
 
+    // Constructs a GtkBuilder instance.
+    GtkBuilder* builder = gtk_builder_new();
+
+    // Loads the UI description.
+    // (Exits if an error occurs.)
+    GError* error = NULL;
+    if (gtk_builder_add_from_file(builder, "window.glade", &error) == 0)
+    {
+        g_printerr("Error loading file: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
+    }
+
+    app_widgets *widgets = g_slice_new(app_widgets);
+
     css();
+    
+    // Gets the widgets.
+    GtkWidget* window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    widgets->w_dlg_file_choose = GTK_WIDGET(gtk_builder_get_object(builder, "dlg_file_choose"));
+    widgets->w_img_main = GTK_IMAGE(gtk_builder_get_object(builder, "img_main"));
 
-    //SET CONTAINERS
-    window = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
-    gtk_window_set_title(GTK_WINDOW(window), "OCR");
-    gtk_container_set_border_width(GTK_CONTAINER(window), 5);
+    GtkFileChooserButton* openfile = GTK_FILE_CHOOSER_BUTTON(gtk_builder_get_object(builder, "openfile"));
+    GtkButton* grayscale = GTK_BUTTON(gtk_builder_get_object(builder, "grayscale"));
+    GtkButton* blackandwhite = GTK_BUTTON(gtk_builder_get_object(builder, "blackandwhite"));
+    GtkButton* segmentation = GTK_BUTTON(gtk_builder_get_object(builder, "segmentation"));
+    GtkButton* ocr = GTK_BUTTON(gtk_builder_get_object(builder, "ocr"));
+    GtkButton* text = GTK_BUTTON(gtk_builder_get_object(builder, "text"));
+    GtkButton* save = GTK_BUTTON(gtk_builder_get_object(builder, "save"));
+    GtkButton* quit = GTK_BUTTON(gtk_builder_get_object(builder, "quit"));
+    //GtkImage* image = GTK_IMAGE(gtk_builder_get_object(builder, "image"));
 
-    vbox = gtk_box_new(TRUE, 10);
-    hpaned = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
+    gtk_builder_connect_signals(builder, widgets);
+    g_object_unref(builder);
 
-    gtk_paned_pack1(GTK_PANED(hpaned), vbox, TRUE, FALSE);
-
-    image = gtk_image_new_from_file(img);
-    gtk_paned_pack2 (GTK_PANED(hpaned), image, TRUE, FALSE);
-
-    gtk_container_add(GTK_CONTAINER(window), hpaned);
-
-    //DEFINE BUTTONS
-    openfile = gtk_button_new_with_label("Open File");
-    grayscale = gtk_button_new_with_label("Grayscale");
-    blackandwhite = gtk_button_new_with_label("Black and White");
-    segmentation = gtk_button_new_with_label("Segmentation");
-    OCR = gtk_button_new_with_label("OCR");
-    text = gtk_button_new_with_label("View Text");
-    save = gtk_button_new_with_label("Save");
-    quit = gtk_button_new_with_label("Quit");
-    gtk_widget_set_name(quit, "quit");
-
-    //PACK BUTTONS TO BOX
-    gtk_box_pack_start(GTK_BOX(vbox), openfile, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), grayscale, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), blackandwhite, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), segmentation, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), OCR, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), text, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), save, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), quit, TRUE, TRUE, 0);
+    GtkImage* img = "OCRtext3.bmp";
 
     //CONNECT SIGNAL
-    g_signal_connect(G_OBJECT(window), "destroy",G_CALLBACK(gtk_main_quit), G_OBJECT(window));
-    g_signal_connect(openfile, "clicked", G_CALLBACK(open_file), image);
+    g_signal_connect(window, "destroy",G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(grayscale, "clicked", G_CALLBACK(gtk_grayscale), img);
     g_signal_connect(blackandwhite, "clicked", G_CALLBACK(gtk_blackwhite), img);
     g_signal_connect(segmentation, "clicked", G_CALLBACK(gtk_segmentation), img);
-    g_signal_connect(OCR, "clicked", G_CALLBACK(open_file), img);
-    g_signal_connect(text, "clicked", G_CALLBACK(open_file), image);
-    g_signal_connect(save, "clicked", G_CALLBACK(open_file), image);
+    g_signal_connect(ocr, "clicked", G_CALLBACK(ocr), NULL);
+    //g_signal_connect(text, "clicked", G_CALLBACK(open_file), image);
+    //g_signal_connect(save, "clicked", G_CALLBACK(open_file), image);
     g_signal_connect(quit, "clicked",G_CALLBACK(gtk_main_quit), G_OBJECT(window));
 
-    gtk_widget_show_all(GTK_WIDGET(window));
-
     gtk_main();
+    g_slice_free(app_widgets, widgets);
 
     return 0;
 }
